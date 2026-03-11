a2e8ae10998170a36ccb4d1216b25ca85b9579fd
pelcman <pelcman.hz@proton.me>
2026-03-12 03:52:24 +0900

    fix Chat

--- templates/game.php
+++ templates/game.php
@@ -16,21 +16,20 @@
 </head>
 <body id="body">
     <div class="clientPortable <?= htmlspecialchars($classClient) ?>">
-        <a href="#" class="ui_rightbtn" id="ui_right_chat" onclick="toggle_visibility('guide');"></a>
-        <a href="#" class="ui_rightbtn" id="ui_right_map" onclick="toggle_visibility('spellbook');"></a>
-        <a href="/logout" class="ui_rightbtn" id="ui_right_logout"></a>
-
         <?php require __DIR__ . '/partials/guide.php'; ?>
         <?php require __DIR__ . '/partials/spellbook.php'; ?>
-
-        <?php require_once __DIR__ . '/partials/audio.php'; ?>
-        <?php renderAudioPlayer('game', $state['floor']); ?>
 
         <script>
             Merveilles.setViewport(<?= $width ?>, <?= $height ?>);
             Merveilles.init(<?= json_encode($data, JSON_THROW_ON_ERROR) ?>);
         </script>
     </div>
+
+    <a href="#" class="ui_rightbtn" id="ui_right_chat" onclick="toggle_visibility('guide');"></a>
+    <a href="#" class="ui_rightbtn" id="ui_right_map" onclick="toggle_visibility('spellbook');"></a>
+    <a href="/logout" class="ui_rightbtn" id="ui_right_logout"></a>
+    <?php require_once __DIR__ . '/partials/audio.php'; ?>
+    <?php renderAudioPlayer('game', $state['floor']); ?>
     <menu></menu>
 </body>
 </html>
